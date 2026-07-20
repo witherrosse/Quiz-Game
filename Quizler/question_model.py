8f59808b4ce1b790f78b272dc1ce8f5bf216e4e2
Ali Fooladi <alifooladiwither@gmail.com>
2026-07-20 11:15:23 +0330

    Update question_model.py

--- Quizler/question_model.py
+++ Quizler/question_model.py
@@ -1,13 +1,15 @@
 class Question:
 
-    ### This function runs when a new Question object is created ###
+    
 
     def __init__(self, q_text, q_answer):
 
-        ### Store the question text ###
+        ''' This function runs when a new Question object is created '''
+
+        
 
         self.text = q_text
 
-        #### Store the correct answer (True/False) ###
+        
 
         self.answer = q_answer
