3d3f3b2b4343d070eced60f5a5833dae309e09b6
Ali Fooladi <alifooladiwither@gmail.com>
2026-07-20 11:16:57 +0330

    Update quiz_brain.py

--- Quizler/quiz_brain.py
+++ Quizler/quiz_brain.py
@@ -2,24 +2,30 @@ import html
 
 class QuizBrain:
 
-    ### Initialize the quiz with a list of questions ###
+    
 
     def __init__(self, q_list):
 
-        self.question_number = 0      ### Track which question we are on ##
-        self.score = 0                ### Track how many correct answers ###
-        self.question_list = q_list   ### All questions in the quiz ###
-        self.current_question = None  ### The question currently being asked ###
+        ''' Initialize the quiz with a list of questions '''
 
-    ### Check if there are more questions to ask ###
+        self.question_number = 0      
+        self.score = 0                
+        self.question_list = q_list   
+        self.current_question = None  
+
+    
 
     def still_has_questions(self):
 
+        ''' Check if there are more questions to ask '''
+
         return self.question_number < len(self.question_list)
 
-    ### Get the next question and return it as a string ###
+    
 
     def next_question(self):
+
+        ''' Get the next question and return it as a string '''
 
         self.current_question = self.question_list[self.question_number]
         self.question_number += 1
@@ -30,13 +36,15 @@ class QuizBrain:
 
         return f"Q.{self.question_number} : {q_text}"
 
-    ###  Check if the user's answer matches the correct answer ###
+    
 
     def check_answer(self, user_answer):
 
+        ''' Check if the user's answer matches the correct answer '''
+
         correct_answer = self.current_question.answer
 
-        ###  Compare answers (case insensitive) ###
+        
 
         if user_answer.lower() == correct_answer.lower():
 
